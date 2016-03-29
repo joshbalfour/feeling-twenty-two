3cc1314a82625e596632eccbc2791db957bfffb0
Josh Balfour <josh@joshbalfour.co.uk>
2016-03-29 17:25:35 +0100

    ES6 for the sake of it

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-module.exports = function(thing){
+(function(){
+	"use strict";
 
-	var thingsThatFeel22 = [
-		22, 
-		"22", 
-		"twenty two", 
-		"twenty-two", 
-		"https://www.youtube.com/watch?v=AgFeZr5ptV8", 
-		"https://en.wikipedia.org/wiki/22_(Taylor_Swift_song)",
-		"https://itun.es/gb/abOeI?i=571637873"
-	];
+	const isTwentyTwo = require('is-twenty-two');
 
-	return thingsThatFeel22.indexOf(thing) > -1;
-};
+	module.exports = function(thing){
+
+		var thingsThatFeel22 = [
+			"https://www.youtube.com/watch?v=AgFeZr5ptV8", 
+			"https://en.wikipedia.org/wiki/22_(Taylor_Swift_song)",
+			"https://itun.es/gb/abOeI?i=571637873"
+		];
+
+		return isTwentyTwo(thing) || thingsThatFeel22.indexOf(thing) > -1;
+	};
+
+})();
